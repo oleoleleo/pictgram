033795345b21fda939543b701d048f785fb21ef5
Leo Takahashi <leotakahashi@Leos-MBA.local>
2019-04-03 23:54:24 +0900

    delete

--- main/pictgram_practice/app/controllers/pages_controller.rb
+++
@@ -1,8 +0,0 @@
-class PagesController < ApplicationController
-  def index
-  end
-
-  def helper
-  end
-  
-end
--- main/pictgram_practice/config/initializers/time_formats.rb
+++
@@ -1,2 +0,0 @@
-Time::DATE_FORMATS[:without_second] = '%Y/%m/%d %H:%M'
-Time::DATE_FORMATS[:extra_short] = '%Y-%m-%d'
